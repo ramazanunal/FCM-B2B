e063bfc998684ac2185a87fe8bae7737da028cfb
gokce <gokcetascii@gmail.com>
2024-04-25 13:23:24 +0300

    bg image added

--- components/Homepage/index.js
+++ components/Homepage/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Header from '../Header'
+import Header from '../Header';
 
 function Homepage() {
   return (
-    <div className='flex items-center justify-center'><Header/></div>
-  )
+    <div className="flex items-center justify-center bg-[url('https://caliskanari.com/wp-content/uploads/2018/10/clothes-2049536750.jpg.webp')] ">
+      <Header />
+    </div>
+  );
 }
 
-export default Homepage
+export default Homepage;
