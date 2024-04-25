b0d34544e4e1ccd08d6d7ff873bc260b6065f6d2
gokce <gokcetascii@gmail.com>
2024-04-25 16:57:23 +0300

    added footer and banner pages

--- components/Homepage/index.js
+++ components/Homepage/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import Header from '../Header';
-import SliderComponent from '../SliderComponent';
+import React from "react";
+import Header from "../Header";
+import SliderComponent from "../SliderComponent";
+import Footer from "../Footer";
+import Banner from "../Banner";
 
 function Homepage() {
   return (
-    <div className="flex flex-col items-center justify-center bg-[url('https://caliskanari.com/wp-content/uploads/2018/10/clothes-2049536750.jpg.webp')] ">
-      <div>
+    <div className="flex flex-col items-center justify-center bg-[url('https://caliskanari.com/wp-content/uploads/2018/10/clothes-2049536750.jpg.webp')]">
+      <div className="bg-white">
         <Header />
       </div>
-      <div>
-        <SliderComponent/>
+      <div className="bg-white">
+        <SliderComponent />
       </div>
+      <div className="bg-white">
+        <Footer />
+      </div>
+      <Banner/>
     </div>
   );
 }
