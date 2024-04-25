547f59f0d6be4483051bcdf682adc2400d0b2fbb
gokce <gokcetascii@gmail.com>
2024-04-25 15:56:56 +0300

    slider page added

--- components/Homepage/index.js
+++ components/Homepage/index.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
 import Header from '../Header';
+import SliderComponent from '../SliderComponent';
 
 function Homepage() {
   return (
-    <div className="flex items-center justify-center bg-[url('https://caliskanari.com/wp-content/uploads/2018/10/clothes-2049536750.jpg.webp')] ">
-      <Header />
+    <div className="flex flex-col items-center justify-center bg-[url('https://caliskanari.com/wp-content/uploads/2018/10/clothes-2049536750.jpg.webp')] ">
+      <div>
+        <Header />
+      </div>
+      <div>
+        <SliderComponent/>
+      </div>
     </div>
   );
 }
