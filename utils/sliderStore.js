97da3c2ede4460a000521155a96f5237cd003710
gokce <gokcetascii@gmail.com>
2024-04-25 15:56:09 +0300

    slider store created with zustand

---
+++ utils/sliderStore.js
@@ -0,0 +1,26 @@
+import create from 'zustand';
+
+const sliderStore = create((set) => ({
+  images: [
+    {
+      id: 1,
+      src: 'https://caliskanari.com/wp-content/uploads/2016/10/1.png',
+      alt: '1',
+      title: '1',
+    },
+    {
+      id: 2,
+      src: 'https://caliskanari.com/wp-content/uploads/2016/10/2.png.webp',
+      alt: '2',
+      title: '2',
+    },
+    {
+      id: 3,
+      src: 'https://caliskanari.com/wp-content/uploads/2016/10/3.png.webp',
+      alt: '3',
+      title: '3',
+    },
+  ],
+}));
+
+export default sliderStore;
