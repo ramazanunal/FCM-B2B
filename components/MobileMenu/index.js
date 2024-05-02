155a5565c6039b08b1da3debbbfa0a2341e4ecf2
gokce <gokcetascii@gmail.com>
2024-05-02 20:42:31 +0300

    style edited

--- components/MobileMenu/index.js
+++ components/MobileMenu/index.js
@@ -2,8 +2,8 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { AiOutlineClose } from "react-icons/ai";
 import { IoIosArrowForward } from "react-icons/io";
+import { FaTimes } from "react-icons/fa";
 
 const MobileMenu = ({ header }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -26,6 +26,7 @@ const MobileMenu = ({ header }) => {
       setSelectedSubMenu(index);
     }
   };
+  
 
   return (
     <div className="block xl:hidden">
@@ -34,13 +35,14 @@ const MobileMenu = ({ header }) => {
       </button>
       {isMenuOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 z-50">
-          <div className="absolute top-0 left-56 mt-2 mr-4 z-30">
+          
+          <div className="absolute left-0 top-0 h-full w-64 bg-white w-[300px] overflow-y-auto">
+          <div className=" mt-2 mr-4 h-[70px] flex justify-end">
             <button onClick={() => setIsMenuOpen(false)}>
-              <AiOutlineClose className="w-[15px] h-[15px] fill-BaseDark" />
+            <FaTimes className=" w-[20px] h-[20px] text-[#555555] hover:text-red-600 hover:scale-110 transition duration-300 ease-in-out transform " />
             </button>
           </div>
-          <div className="absolute left-0 top-0 h-full w-64 bg-white">
-            <div id="mainmenuitem" className=" pb-24">
+            <div id="mainmenuitem">
               <ul className="flex flex-col text-CustomGray">
                 {header.mainMenuItems.map((mainMenuItem, index) => (
                   <li
@@ -48,17 +50,19 @@ const MobileMenu = ({ header }) => {
                     className="relative mr-[25px] leading-[1.3] mx-2"
                   >
                     <div
-                      className="flex flex-row items-center justify-center"
+                      className="flex flex-row items-center justify-center "
                       onClick={() => toggleMainMenu(index)}
                     >
-                      <span className="w-[53px] h-[53px] ">
+                      <div className="py-[15px] flex flex-row items-center justify-center">
+                      <span className="w-[53px] h-[53px] flex flex-row items-center justify-center">
                         {mainMenuItem.icon}
                       </span>
-                      <span className="uppercase text-[12px] font-bold tracking-[1px] pb-[15px] hover:text-LightBlue transition duration-300 ease-in-out transform uppercase ">
+                      <span className="uppercase text-[13px] font-extrabold tracking-[1px] pl-[15px] hover:text-LightBlue transition duration-300 ease-in-out transform uppercase ">
                         {mainMenuItem.text}
                       </span>
+                      </div>
                       {mainMenuItem.subMenus.length > 0 && (
-                        <IoIosArrowForward className="w-5 h-5 text-black" />
+                        <IoIosArrowForward className="w-4 h-4 text-black " />
                       )}
                     </div>
                     {selectedMainMenu === index && (
@@ -74,16 +78,17 @@ const MobileMenu = ({ header }) => {
                 ))}
               </ul>
             </div>
+            <div className="my-[30px]">
             <ul>
               {header.menus.map((menu, index) => (
-                <li key={menu.id}>
+                <li className="flex flex-row items-center py-[15px] px-[30px] text-LightBlue text-[13px] font-extrabold uppercase flex justify-between " key={menu.id}>
                   <Link href={menu.href}>{menu.text}</Link>
                   {menu.subMenus.length > 0 && (
                     <div
                       onClick={() => toggleSubMenu(index)}
                       className="cursor-pointer"
                     >
-                      <IoIosArrowForward className="w-5 h-5 text-black" />
+                      <IoIosArrowForward className="w-4 h-4 text-black fill-LightBlue" />
                     </div>
                   )}
                   {selectedSubMenu === index && (
@@ -98,6 +103,7 @@ const MobileMenu = ({ header }) => {
                 </li>
               ))}
             </ul>
+            </div>
           </div>
         </div>
       )}
