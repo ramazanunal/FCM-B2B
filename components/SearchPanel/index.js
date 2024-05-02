d4bbb9df17757408475aa474c4eb443bef677ac3
gokce <gokcetascii@gmail.com>
2024-05-03 02:14:46 +0300

    responsive added

--- components/SearchPanel/index.js
+++ components/SearchPanel/index.js
@@ -14,40 +14,45 @@ function SearchPanel({ toggleSearchPanel }) {
   };
 
   return (
-    <div className="w-full h-full fixed top-0 left-0 right-0 z-50 bg-gray-800 bg-opacity-75 flex items-center justify-center transition-transform duration-500 ease-in-out">
+    <div className="w-full h-screen fixed top-0 left-0 right-0 z-50 bg-gray-800 bg-opacity-75 flex items-center justify-center transition-transform duration-500 ease-in-out">
       <Formik
         initialValues={{ s: "" }}
         validationSchema={validationSchema}
         onSubmit={handleSubmit}
       >
         {({ errors, touched }) => (
-          <div className="fixed top-0 left-0 right-0 z-50 bg-white h-[165px] flex items-center justify-center flex-col">
-            <Form id="ajax-search" className="container mx-auto px-[15px] ">
-              <div className="w-[1170px]">
-                <div className="relative text-[10px] uppercase text-[#555555] h-[57px] flex items-center ">
-                  What are you looking for?
+          <div className="fixed top-0 left-0 right-0 z-50 bg-white w-full h-[55px] md:h-[165px] flex items-center justify-center flex-col">
+            <Form
+              id="ajax-search"
+              className="container mx-auto px-[30px] md:px-[15px] "
+            >
+              <div className="max-w-[1250px]">
+                <div className="relative text-[10px] uppercase text-[#555555] h-0 md:h-[57px] flex items-center ">
+                  <span className="hidden md:flex">
+                    What are you looking for?
+                  </span>
                   <button
-                    className="absolute top-0 right-0 "
+                    className="absolute top-1 md:top-0 right-0 "
                     type="button"
-                    onClick={toggleSearchPanel} // Close button click handler
+                    onClick={toggleSearchPanel}
                   >
-                    <FaTimes className=" w-[20px] h-[20px] text-[#555555] hover:text-red-600 hover:scale-110 transition duration-300 ease-in-out transform " />
+                    <FaTimes className="w-[20px] h-[20px] text-[#555555] hover:text-red-600 hover:scale-110 transition duration-300 ease-in-out transform" />
                   </button>
                 </div>
 
-                <div className="relative border-b border-[#e4e4e4] ">
+                <div className="relative md:border-b border-[#e4e4e4] ">
                   <Field
                     name="searchinput"
                     placeholder="Search products..."
-                    className={`form-control w-[1140px] pb-[15px] pr-[30px] text-[30px] font-bold lowercase ${
+                    className={`form-control w-[350px] md:max-w-[1140px] md:pb-[15px] pr-[30px] text-[15px] md:text-[30px] md:font-bold lowercase outline-none ${
                       errors.s && touched.s ? "is-invalid" : ""
                     }`}
                   />
                   <button
                     type="submit"
-                    className="search absolute right-0 top-5"
+                    className="search absolute right-10 md:right-0 top-1 md:top-5"
                   >
-                    <FaSearch className=" w-[26px] h-[26px] text-[#555555] hover:text-LightBlue hover:scale-110 transition duration-300 ease-in-out transform " />
+                    <FaSearch className="w-[19px] md:w-[26px] h-[19px] md:h-[26px] text-[#555555] hover:text-LightBlue hover:scale-110 transition duration-300 ease-in-out transform " />
                   </button>
                 </div>
               </div>
