f3d83def986589e2ee5d6fe05af14ad78bf37d49
gokce <gokcetascii@gmail.com>
2024-05-03 17:10:53 +0300

    added filter for products list and style editing

--- components/SearchPanel/index.js
+++ components/SearchPanel/index.js
@@ -1,36 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 import { FaSearch, FaTimes } from "react-icons/fa";
+import categoryStore from "@/utils/categoryStore";
+import Image from "next/image";
+import Link from "next/link";
 
 function SearchPanel({ toggleSearchPanel }) {
+  const [searchResults, setSearchResults] = useState([]);
+
   const validationSchema = Yup.object().shape({
-    s: Yup.string().required("Please enter a search term"),
+    searchinput: Yup.string().required(""),
   });
 
   const handleSubmit = (values, { resetForm }) => {
-    console.log("Submitted search term:", values.s);
+    const searchTerm = values.searchinput.toLowerCase();
+    const results = [];
+
+    categoryStore.getState().categories.forEach((category) => {
+      category.products.forEach((product) => {
+        if (product.name.toLowerCase().includes(searchTerm)) {
+          results.push(product);
+        }
+      });
+    });
+
+    setSearchResults(results);
     resetForm();
   };
 
+  useEffect(() => {
+    // Başlangıçta bir arama yapmak için
+    const initialSearchTerm = "initial search term"; // İstediğiniz varsayılan arama terimi
+    handleSubmit({ searchinput: initialSearchTerm }, { resetForm: () => {} });
+  }, []); // Boş bağımlılık dizisi, yalnızca bileşen monte edildiğinde bir kere çalışır
+
   return (
-    <div className="w-full h-screen fixed top-0 left-0 right-0 z-50 bg-gray-800 bg-opacity-75 flex items-center justify-center transition-transform duration-500 ease-in-out">
+    <div className="w-full flex flex-col items-center justify-center transition-transform duration-500 ease-in-out shadow-[0_5px_20px_rgba(0,0,0,0.3)] py-[15px] z-[1000] rounded-xl ">
       <Formik
-        initialValues={{ s: "" }}
+        initialValues={{ searchinput: "" }}
         validationSchema={validationSchema}
         onSubmit={handleSubmit}
       >
         {({ errors, touched }) => (
-          <div className="fixed top-0 left-0 right-0 z-50 bg-white w-full h-[55px] md:h-[165px] flex items-center justify-center flex-col">
+          <div className="bg-white w-[400px]  flex items-center justify-center flex-col">
             <Form
               id="ajax-search"
               className="container mx-auto px-[30px] md:px-[15px] "
             >
-              <div className="max-w-[1250px]">
-                <div className="relative text-[10px] uppercase text-[#555555] h-0 md:h-[57px] flex items-center ">
-                  <span className="hidden md:flex">
-                    What are you looking for?
-                  </span>
+              <div>
+                <div className="relative text-[10px] uppercase text-[#555555] pt-10 flex items-center ">
                   <button
                     className="absolute top-1 md:top-0 right-0 "
                     type="button"
@@ -40,27 +59,60 @@ function SearchPanel({ toggleSearchPanel }) {
                   </button>
                 </div>
 
-                <div className="relative md:border-b border-[#e4e4e4] ">
+                <div className="relative border-2 border-LightBlue rounded-full  py-2 pr-2 hover:border-DarkBlue transition duration-300 ease-in-out transform pl-4">
                   <Field
                     name="searchinput"
                     placeholder="Search products..."
-                    className={`form-control w-[350px] md:max-w-[1140px] md:pb-[15px] pr-[30px] text-[15px] md:text-[30px] md:font-bold lowercase outline-none ${
-                      errors.s && touched.s ? "is-invalid" : ""
+                    className={`form-control text-[15px] w-[310px] flex items-start font-bold lowercase outline-none ${
+                      errors.searchinput && touched.searchinput
+                        ? "is-invalid"
+                        : ""
                     }`}
                   />
                   <button
                     type="submit"
-                    className="search absolute right-10 md:right-0 top-1 md:top-5"
+                    className="search absolute right-3 top-2"
                   >
-                    <FaSearch className="w-[19px] md:w-[26px] h-[19px] md:h-[26px] text-[#555555] hover:text-LightBlue hover:scale-110 transition duration-300 ease-in-out transform " />
+                    <FaSearch className="w-[19px] h-[19px]  text-[#555555] hover:text-LightBlue hover:scale-110 transition duration-300 ease-in-out transform " />
                   </button>
                 </div>
               </div>
-              {errors.s && touched.s && <div>{errors.s}</div>}
+              {errors.searchinput && touched.searchinput && (
+                <div>{errors.searchinput}</div>
+              )}
             </Form>
           </div>
         )}
       </Formik>{" "}
+      <div>
+        {searchResults.length > 0 && (
+          <div className="w-[400px] mt-4 overflow-y-auto max-h-80 ">
+            <ul>
+              {searchResults.map((product) => (
+                <li
+                  className="p-5 border-b border-LightBlue flex flex-row justify-start gap-4"
+                  key={product.id}
+                >
+                  <Image
+                    src={product.imagesrc}
+                    width={70}
+                    height={70}
+                    alt={product.name}
+                  />
+                  <Link
+                    href={product.link}
+                    className="flex items-center"
+                  >
+                    <span className=" flex text-start">
+                      <p className="font-bold text-[16px] text-CustomGray hover:scale-105 hover:text-LightBlue transition-all transform easy-in-out duration-500">{product.name}</p>
+                    </span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
