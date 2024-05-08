a63f29bd18781e32420142e34b16e6b9dce4250d
gokce <gokcetascii@gmail.com>
2024-05-08 17:49:24 +0300

    added header, footer and banner

--- containers/Home/index.jsx
+++ containers/Home/index.jsx
@@ -1,26 +1,17 @@
 import React from "react";
-import Header from "@/components/Header";
 import SliderComponent from "@/components/SliderComponent";
 import HomeCategories from "@/components/HomeCategories";
-import Footer from "@/components/Footer";
-import Banner from "@/components/Banner";
+
 
 const HomeContainer = () => {
   return (
-    <div className=" flex flex-col items-center  bg-[url('https://caliskanari.com/wp-content/uploads/2018/10/clothes-2049536750.jpg.webp')] bg-no-repeat bg-[#6bcdec]">
-      <div className="bg-white">
-        <Header />
-      </div>
+    <div className=" flex flex-col items-center  ">
       <div className="bg-white">
         <SliderComponent />
       </div>
       <div className="bg-white">
         <HomeCategories />
       </div>
-      <div className="bg-white">
-        <Footer />
-      </div>
-      <Banner />
     </div>
   );
 };
