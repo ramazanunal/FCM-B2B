35d4b5b928c0b2ef3cbc3adcf049074da497cd15
gokce <gokcetascii@gmail.com>
2024-05-08 17:48:58 +0300

    cart container page created and added shopping cart

---
+++ containers/Cart/index.jsx
@@ -0,0 +1,11 @@
+import ShoppingCart from '@/components/ShoppingCart'
+import React from 'react'
+
+function CartContainer() {
+  return (
+    <div className="bg-white"><ShoppingCart/></div>
+
+  )
+}
+
+export default CartContainer
