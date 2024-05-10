00fbaaa3eab6f99baf52ba0787b5ea9f43f143f4
gokce <gokcetascii@gmail.com>
2024-05-10 16:39:20 +0300

    created order information store for order address

---
+++ utils/orderInformationStore.js
@@ -0,0 +1,23 @@
+import create from 'zustand';
+
+const orderInformationStore = create((set) => ({
+    address: [
+        {
+          id: 1,
+          name: "Ev Adresi",
+          info: "1234 Elm Sokağı Springfield, IL 62701 ",
+        },
+        {
+            id: 2,
+            name: "İş Adresi",
+            info: "5678 Oak Caddesi Springfield, IL 62702",
+          },
+          {
+            id: 2,
+            name: "Ev Adresi 2",
+            info: "5345 Oak Caddesi Springfield, IL 76545",
+          },
+      ],
+}));
+
+export default orderInformationStore;
