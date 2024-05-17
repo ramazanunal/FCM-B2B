ff08382ebcb714188c020d676e662e2ebedd4a8f
gokce <gokcetascii@gmail.com>
2024-05-18 02:59:15 +0300

    added datas

--- utils/orderInformationStore.js
+++ utils/orderInformationStore.js
@@ -1,21 +1,25 @@
 import { create } from 'zustand'
-
+import { FcHome } from "react-icons/fc";
+import { FcBusiness } from "react-icons/fc";
 const orderInformationStore = create((set) => ({
     address: [
         {
           id: 1,
           name: "Ev Adresi",
-          info: "1234 Elm Sokağı Springfield, IL 62701 ",
+          info: "1234 Elm Sokağı Springfield, IL Nowakoscka, 31-215 Cracow-Poland",
+          icon: <FcHome/>
         },
         {
             id: 2,
             name: "İş Adresi",
-            info: "5678 Oak Caddesi Springfield, IL 62702",
+            info: "5678 Oak Caddesi Springfield, IL Nowakoscka, 32-567 Cracow-Poland",
+            icon: <FcBusiness/>
           },
           {
             id: 2,
             name: "Ev Adresi 2",
-            info: "5345 Oak Caddesi Springfield, IL 76545",
+            info: "5345 Oak Caddesi Springfield, IL Nowakoscka,56-893 Cracow-Poland",
+            icon: <FcHome/>
           },
       ],
 }));
