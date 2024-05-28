83e00d784f3f887babb9d3f267e49b24e6581317
gokce <gokcetascii@gmail.com>
2024-05-28 16:47:23 +0300

    backend example

---
+++ pages/api/urun/index.js
@@ -0,0 +1,17 @@
+import {getAllData} from '@/services/serviceOperations';
+
+const handler = async (req, res) => {
+
+    if(req.method === "POST") {
+        return res.status(200).json({message: "Method POST"})
+    }
+    if(req.method === "GET") {
+        const data = await getAllData('STKKART');
+        console.log("data: ",data)
+
+        return res.status(200).json({message: "Method GET", data})
+    }
+
+}
+
+export default handler;
