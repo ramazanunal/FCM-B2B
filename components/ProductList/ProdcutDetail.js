a54e77ff18bfe2159c28356095c1fce65f13baac
Ramazan Ünal <unalramaz@gmail.com>
2024-07-21 13:03:17 +0300

    product detail some style changes

--- components/ProductList/ProdcutDetail.js
+++ components/ProductList/ProdcutDetail.js
@@ -28,11 +28,11 @@ function ProdcutDetail({ product }) {
     );
   }
   return (
-    <div className="flex flex-col justify-center items-center bg-[url('/backgroundImage.webp')] bg-no-repeat  bg-contain bg-[#6bcdec]">
+    <div className="flex flex-col min-h-screen justify-center items-center bg-[url('/backgroundImage.webp')] bg-no-repeat  bg-contain bg-[#6bcdec]">
       <div className="bg-gray-50 ">
         <div className="grid grid-rows-2 px-5 lg:w-[1188px]  md:px-14 pt-14 pb-3 mb-2  lg:mx-auto">
           <div className="row-span-1 md:grid md:grid-cols-4 md:grid-flow-col ">
-            <div className="col-span-2 flex md:justify-center border border-dashed ">
+            <div className="col-span-2 flex md:justify-center border border-dashed rounded-lg">
               <div className=" max-w-xl">
                 <img
                   className="max-h-96"
